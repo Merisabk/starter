88d6d793025fb47cd7333f17e05de0ab04cacd8d
Merisa <bishwakarma.merisa@gmail.com>
2022-06-09 12:14:19 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <p className="position-absolute top-0 start-50 translate-middle">QA Ltd 2019-</p>
+        </footer>
+
+        </>
+    )
+}
+
+export default Footer;
