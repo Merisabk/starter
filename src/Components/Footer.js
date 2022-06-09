d3ff3901f0ee25683d57b856bd9944d546030ce2
Merisa <bishwakarma.merisa@gmail.com>
2022-06-09 12:43:52 +0100

    sticky footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../App.css';
 
 const Footer = () => {
     return (
         <>
-        <footer>
-            <p className="position-absolute top-0 start-50 translate-middle">QA Ltd 2019-</p>
+        <footer className="container foot" >
+            <p className="text-center mt-1 pl-1 mr-1">QA Ltd 2019-</p>
         </footer>
 
         </>
